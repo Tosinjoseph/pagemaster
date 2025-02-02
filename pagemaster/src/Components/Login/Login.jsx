39ed4af0d89373ffac27f05e761ba12fc7d46eba
TosinJoseph <tosinjosephoffice2@gmail.com>
2025-02-02 15:16:46 -0800

    second commit

--- pagemaster/src/Components/Login/Login.jsx
+++ pagemaster/src/Components/Login/Login.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import form_back from '../../assets/form-back.png'
 
 const Login = ({formShow,setFormShow}) => {
+  const[changeForm, setChangeForm] = useState("Sign Up")
   return (
     <div className={`login-container ${formShow? '': 'hide'}`} >
         <form>
+                 <h2>{changeForm}</h2>
                 <label>Your Name</label>
                 <input type='text' name='name' placeholder='Input Your Name' required/>
-                <label>Your Phone Number</label>
-                <input type='number' name='phone' placeholder='Input Your Mobile Number' required/>
-                <button type='submit' className='btn1 form-btn'>Submit now</button>
+                <label>Your Password</label>
+                <input type='password' name='password' placeholder='Input Your Password' required/>
+               {changeForm==='Login' ? <div></div> : <label>Your Email</label>}
+               {changeForm==='Login' ? <div></div> : <input type='email' name='email' placeholder='Input Your Email' required/> }
+               {changeForm==='Sign Up' ? <div></div> : <p>Lost password?  <a href =''> Click here</a></p>}
+                <div className="two-button">
+                <button onClick={()=> setChangeForm('Sign Up')} type='submit' className={changeForm==='Sign Up'?'grey-btn btn1 form-btn':'btn1 form-btn' }>Sign Up</button>
+                <button onClick={()=> setChangeForm('Login')} type='submit' className={changeForm==='Login'?'grey-btn btn1 form-btn':'btn1 form-btn' }>Login</button>
+                </div>
                  </form>
            <div className='form-back' onClick={()=>setFormShow(false)}><img src={form_back} alt=''/></div>
       
